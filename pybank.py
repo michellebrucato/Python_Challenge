e4ba96f1770e91e93f9a05fac32180d3ace880d9
Michelle Brucato <MichelleRBrucato@gmail.com>
2019-12-01 20:21:14 -0600

    calculate total months and total profit.

--- pybank.py
+++ pybank.py
@@ -5,9 +5,22 @@ filepath = os.path.join("..", "Python_Challenge", "budget_data.csv")
 
 with open(filepath, "r") as in_file:
     in_csv = csv.reader(in_file)
+    csv_header = next(in_csv)
+    # print(f"Header: {csv_header}")
 
-    header = next(in_csv)
-    print(header)
+    profit = []
+    months = []
+    revenue_change = []
+
+    for row in in_csv:
+        profit.append(int(row[1]))
+        months.append(row[0])
     
-    # for row in in_csv:
-    #     print(row)
+    total_months = len(months)
+    # print(total_months)
+    total_profit = sum(profit)
+    # print(total_profit)
+
+
+
+
