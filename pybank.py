a60cddb71c98593419a696d248c618c347fd5051
Michelle Brucato <MichelleRBrucato@gmail.com>
2019-12-01 21:49:12 -0600

    calculate and print out results to financial analysis.

--- pybank.py
+++ pybank.py
@@ -12,20 +12,36 @@ with open(filepath, "r") as in_file:
     months = []
     revenue_change = []
 
+
     for row in in_csv:
         profit.append(int(row[1]))
         months.append(row[0])
     
     total_months = len(months)
-    # print(total_months)
     total_profit = sum(profit)
-    # print(total_profit)
-
+    
     for e in range(1, len(profit)):
         revenue_change.append((int(profit[e]) - int(profit[e-1])))
     
     revenue_average = sum(revenue_change) / len(revenue_change)
-    # print(revenue_average)
+    greatest_increase = max(revenue_change)
+    greatest_decrease = min(revenue_change)
+  
+
+    print("Financial Analysis")
+    print("__________________________")
+    print("Total Months: " + str(total_months))
+    print("Total: " + "$" + str(total_profit))
+    print("Average Change: " + "$" + str(round(revenue_average, 2)))
+    print("Greatest Profit Increase: " + str(months[revenue_change.index(max(revenue_change))+1]) + " " + "$" + str(greatest_increase))
+    print("Greatest Profit Decrease: " + str(months[revenue_change.index(min(revenue_change))+1]) + " " + "$" + str(greatest_decrease))
+
+  
+
+
+    
+
+
 
 
 
