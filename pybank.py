3a31419cb9ee20ee3387d04ae7eb548699677f5d
Michelle Brucato <MichelleRBrucato@gmail.com>
2019-12-01 22:29:00 -0600

    add comments to explain code.

--- pybank.py
+++ pybank.py
@@ -13,21 +13,26 @@ with open(filepath, "r") as in_file:
     months = []
     revenue_change = []
 
+
+    # seperate profit and months using append 
     for row in in_csv:
         profit.append(int(row[1]))
         months.append(row[0])
     
+    # calculate total months and total profit 
     total_months = len(months)
     total_profit = sum(profit)
     
+    # calculate change in revenue 
     for e in range(1, len(profit)):
         revenue_change.append((int(profit[e]) - int(profit[e-1])))
     
+    # calculate average revenue change, greatest increase and greatest decrease 
     revenue_average = sum(revenue_change) / len(revenue_change)
     greatest_increase = max(revenue_change)
     greatest_decrease = min(revenue_change)
   
-
+    # print out results 
     print("Financial Analysis")
     print("__________________________")
     print("Total Months: " + str(total_months))
@@ -36,6 +41,7 @@ with open(filepath, "r") as in_file:
     print("Greatest Profit Increase: " + str(months[revenue_change.index(max(revenue_change))+1]) + " " + "$" + str(greatest_increase))
     print("Greatest Profit Decrease: " + str(months[revenue_change.index(min(revenue_change))+1]) + " " + "$" + str(greatest_decrease))
 
+# export text file with financial results 
 with open(output_file, "w+") as text_file:
     text_file.write("Financial Analysis\n")
     text_file.write("__________________________\n")
